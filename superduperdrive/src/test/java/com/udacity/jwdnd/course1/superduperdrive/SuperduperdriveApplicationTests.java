cdce0d75ce34a3efa5c337da6ad9b9da9745a216
Thai Nguyen <thainguyen96dtu@gmail.com>
2023-11-30 21:56:20 +0700

    Init source code for Super Duper Drive project

---
+++ superduperdrive/src/test/java/com/udacity/jwdnd/course1/superduperdrive/SuperduperdriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udacity.jwdnd.course1.superduperdrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperduperdriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
